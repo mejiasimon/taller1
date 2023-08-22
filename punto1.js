cd427dafdade6d698afc5c63ecf8430a10ac39dd
mejiasimon <97535209+mejiasimon@users.noreply.github.com>
2023-08-22 14:46:22 -0500

    punto 3

---
+++ punto1.js
@@ -0,0 +1,6 @@
+var coordsX=150
+var coordsY=30
+var coordsX2=6
+var coordsY2=20
+var distancia=(x1,y1,x2,y2)=>{return(resultado=Math.sqrt((Math.pow(x1,2)-Math.pow(y1,2))+(Math.pow(x2,2)-Math.pow(y2,2))))}
+console.log(`${distancia(coordsX,coordsY,coordsX2,coordsY2)} metros de distancia`)
---
+++ punto2.js
@@ -0,0 +1,11 @@
+var accesCodes=["ARQ2555: Sara bel-sun","ARQ2556: nodin chavdi","ARQ2557: finn"]
+var pilots=new pilot(accesCodes)
+function pilot(accescodes){
+    let names=[]
+accescodes.forEach(element => {
+    let obtainName=element.split(":")
+    names.push(obtainName[1])
+});
+return(names)
+}
+console.log(pilots)
